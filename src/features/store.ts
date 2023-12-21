616ea747040e676f9b0664870dfbb4ca5d914a09
IsacJfs <isac.jfs@gmail.com>
2023-12-21 15:26:31 -0300

    :construction: adicionando estado global para o token

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginModalReducer from './slicers/loginModalSlice';
 import registerModalReducer from './slicers/registerSlice';
+import authReducer from './slicers/authSlice';
 
 export const store = configureStore({
   reducer: {
     loginModal: loginModalReducer,
     registerModal: registerModalReducer,
+    auth: authReducer,
   },
 });
 
