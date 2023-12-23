f3ad778ee0e39a600ed6f54d26a8ea0340ccaffc
IsacJfs <isac.jfs@gmail.com>
2023-12-23 09:50:43 -0300

    Add Avatar component and update store configuration

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,14 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginModalReducer from './slicers/loginModalSlice';
 import registerModalReducer from './slicers/registerSlice';
-import authReducer from './slicers/authSlice';
+import userReducer from './slicers/userSlice';
+import profileReducer from './slicers/profileSlice';
+import editReducer from './slicers/editModalSlice';
 
 export const store = configureStore({
   reducer: {
     loginModal: loginModalReducer,
     registerModal: registerModalReducer,
-    auth: authReducer,
+    user: userReducer,
+    profile: profileReducer,
+    editModal: editReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
