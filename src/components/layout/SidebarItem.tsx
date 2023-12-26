c26313b12a21a5ce4f815688606f1dfbfd0d8857
IsacJfs <isac.jfs@gmail.com>
2023-12-26 18:21:31 -0300

    Refactor Sidebar component and SidebarItem component

--- src/components/layout/SidebarItem.tsx
+++ src/components/layout/SidebarItem.tsx
@@ -1,4 +1,5 @@
-import { IconType } from "react-icons";
+import { IconType } from 'react-icons'
+import { useNavigate } from 'react-router-dom'
 
 interface SidebarItemProps {
   label: string
@@ -9,44 +10,34 @@ interface SidebarItemProps {
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
   label,
-  icon: Icon
+  href,
+  icon: Icon,
+  onClick
 }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    if (href) {
+      navigate(href)
+    } else if (onClick) {
+      onClick()
+    }
+  }
+
   return (
-    <div className="flex flex-row items-center">
-      <div className="
-        relative
-        rounded-full
-        h-14
-        w-14
-        flex
-        items-center
-        justify-center
-        p-4
-        hover:bg-slate-300
-        cursor-pointer
-        lg:hidden
-      ">
-        <Icon size={28} color="white"/>
+    <div
+      className="flex flex-row items-center cursor-pointer"
+      onClick={handleClick}
+    >
+      <div className="rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 lg:hidden">
+        <Icon size={28} color="white" />
       </div>
-      <div className="
-        relative
-        hidden
-        lg:flex
-        gap-4
-        p-4
-        rounded-full
-        hover:bg-slate-300
-        hover:bg-opacity-10
-        cursor-pointer
-        items-center
-      ">
-        <Icon size={28} color="white"/>
-        <p className="hidden lg:block text-white text-xl">
-          {label}
-        </p>
+      <div className="hidden lg:flex gap-4 p-4 rounded-full hover:bg-slate-300 hover:bg-opacity-10 items-center">
+        <Icon size={28} color="white" />
+        <p className="text-white text-xl">{label}</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default SidebarItem;
+export default SidebarItem
